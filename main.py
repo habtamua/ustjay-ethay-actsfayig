9f2a793a185683bf45a9e6676081fe34b0f86100
HABTAMU ASFAW <asfawh@uw.edu>
2019-05-05 21:27:50 -0700

    add doc string and update main py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+A Mashup web app code to scrape a random fact from http://unkno.com. 
+and send it to a pig latin web application running on Heroku: https://hidden-journey-62459.herokuapp.com, 
+and print out the address for that piglatinized fact on the home page. 
+"""
 import os
 
 import requests
@@ -13,15 +18,25 @@ FACT_URL = 'http://unkno.com'
 app = Flask(__name__)
 
 def get_fact():
-
+    """ 
+    Retrieves a "fact" from the page: http://unkno.com/
+    :return: fact
+    """
     response = requests.get(FACT_URL)
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
     return facts[0].getText()
 
 def get_url(fact):
+    """
+    Get the URL and display it on home page.
+    to get the URL need to send a request to the Pig-Latin app with the text form data as the payload.
+    The response to this will have the URL that we want to display as an HTTP Header, we also need to disable the redirect request/response.
+    :params: fact
+    :return: url location is the Response headers
+    """
+
     response =  requests.post(URL, data={'input_text':fact}, allow_redirects=False)
 
     return response.headers['Location']
