84b050a44d7d1ba06a4b8ae59440e3364de93dfd
HABTAMU ASFAW <asfawh@uw.edu>
2019-05-05 19:27:05 -0700

    piglatinize works

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+# api-endpoint for piglatinize
+URL = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
 
+# api-endpoint for facts
+FACT_URL = 'http://unkno.com'
+
+app = Flask(__name__)
 
 def get_fact():
 
-    response = requests.get("http://unkno.com")
+    response = requests.get(FACT_URL)
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
     return facts[0].getText()
 
+def get_url(fact):
+    response =  requests.post(URL, data={'input_text':fact}, allow_redirects=False)
+
+    return response.headers['Location']
+
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    piglatinize_url = get_url(fact)
 
+    return Response(response=piglatinize_url, mimetype="text/html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
